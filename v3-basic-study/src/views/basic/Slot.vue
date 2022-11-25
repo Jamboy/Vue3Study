1c74617161bee52e21dd838e8b8a3386a8765b44
Jamboy <jamboylive@gmail.com>
2022-11-25 14:09:40 +0800

    feat: 回退后的修改I

--- v3-basic-study/src/views/basic/Slot.vue
+++ v3-basic-study/src/views/basic/Slot.vue
@@ -3,7 +3,7 @@
  * @FilePath: \Vue3Study\v3-basic-study\src\views\basic\Slot.vue
  * @Author: Jamboy
  * @Date: 2022-10-09 15:06:26
- * @LastEditTime: 2022-10-09 15:11:18
+ * @LastEditTime: 2022-11-25 14:09:24
 -->
 <script lang="ts" setup>
 import Child from './comp/Child.vue';
@@ -11,7 +11,7 @@ import Child from './comp/Child.vue';
 <template>
   <div>slot</div>
   <Child>
-    默认插槽
+    默认插槽范德萨发生
     <template #title>标题</template>
     <template #content>内容</template>
   </Child>
