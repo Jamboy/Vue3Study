669a9a65d4568bab0d67a61f6a013c0c3ae00178
Jamboy <jamboylive@gmail.com>
2022-11-25 14:22:37 +0800

    feat: test1

--- v3-basic-study/src/views/basic/comp/Child.vue
+++ v3-basic-study/src/views/basic/comp/Child.vue
@@ -3,14 +3,14 @@
  * @FilePath: \Vue3Study\v3-basic-study\src\views\basic\comp\Child.vue
  * @Author: Jamboy
  * @Date: 2022-08-25 11:36:57
- * @LastEditTime: 2022-10-09 15:29:20
+ * @LastEditTime: 2022-11-25 14:21:39
 -->
 <script lang="ts" setup>
 import { ref, useCssModule } from 'vue';
 
 const fontColor = ref<string>('red');
 setTimeout(() => (fontColor.value = 'yellow'), 1000);
-
+// 回退后的 修改
 const style = useCssModule();
 
 const content = `<p class="${style.title}">
